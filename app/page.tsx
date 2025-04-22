17d909eec06ca754167808bcc7ce4a290373c346
k-burlai <k.burlai@samsung.com>
2025-04-22 13:55:07 +0200

    html files and simple navigation

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,19 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Development</a>
+          </li>
+          <li>
+            <a href="#">Design</a>
+          </li>
+        </ul>
+      </nav>
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1>Sample Next.js App</h1>
         <Image
